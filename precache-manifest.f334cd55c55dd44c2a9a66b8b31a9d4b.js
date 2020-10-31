b0e8e0ba3215828ea32aeab755c933a5ae09c10a
tinalolo <43947407+tinalolo@users.noreply.github.com>
2020-10-31 17:40:49 +0800

    Add files via upload

---
+++ precache-manifest.f334cd55c55dd44c2a9a66b8b31a9d4b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0841fa04855f47d0367597717414c263",
+    "url": "alarms/alarm.mp3"
+  },
+  {
+    "revision": "7705ff8a183715dd088d29eb12ab5d1d",
+    "url": "alarms/johncena2_short.mp3"
+  },
+  {
+    "revision": "7d61139d73c4f52b12c7bd151099830b",
+    "url": "alarms/supercar_short.mp3"
+  },
+  {
+    "revision": "e01183e7f1ec6e67253c",
+    "url": "css/chunk-vendors.1c6d5458.css"
+  },
+  {
+    "revision": "eeb25cfa9aca29886a02cee15d9b3368",
+    "url": "index.html"
+  },
+  {
+    "revision": "2d58660fb9a98985f52f",
+    "url": "js/app.f14b5d57.js"
+  },
+  {
+    "revision": "e01183e7f1ec6e67253c",
+    "url": "js/chunk-vendors.3bd6c551.js"
+  },
+  {
+    "revision": "3b927401b68fb37f5b7f",
+    "url": "js/list.09e18235.js"
+  },
+  {
+    "revision": "efb574448ee601284743",
+    "url": "js/settings.3cb6f76e.js"
+  },
+  {
+    "revision": "3dc210960fe7e4127773465ac5d8a550",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
